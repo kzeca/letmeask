cefaeefe74f39782e2fd4ed6156da32747c415e2
kzeca <jcpeixotoleao@hotmail.com>
2022-02-01 16:58:41 -0400

    New room page, with firebase authentication and React Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
-    
     <div>
-      <Home />
+      <BrowserRouter> 
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms/new" element={<NewRoom />} />
+          </Routes>
+        </AuthContextProvider>        
+      </BrowserRouter>
     </div>
   );
 }
-
 
 export default App;
 
